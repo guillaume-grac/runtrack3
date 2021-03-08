39cdeddf5adc3fd51dfe439af8165d98a563be61
Guillaume <gui.grc@yahoo.com>
2021-03-08 09:57:46 +0100

    job03 Done !

---
+++ jour01/job03/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Job01</title>
+    <script type='text/javascript' src='script.js'></script>
+</head>
+<body>
+
+</body>
+</html>
