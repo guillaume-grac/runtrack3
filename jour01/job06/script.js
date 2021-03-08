75211b77b4406c8e9ce69fb094a063e7508052cc
Guillaume <gui.grc@yahoo.com>
2021-03-08 11:13:41 +0100

    job06 Done !

---
+++ jour01/job06/script.js
@@ -0,0 +1,16 @@
+function fizzbuzz() {
+
+    for (let i = 0; i < 151; i++){
+        if (i % 3 === 0) {
+            console.log('Fizz');
+        }
+        if (i % 5 === 0) {
+            console.log('Buzz');
+        }
+        if (i % 3 === 0 && i % 5 === 0)
+            console.log('FizzBuzz');
+        else {
+            console.log(i);
+        }
+    }
+}
