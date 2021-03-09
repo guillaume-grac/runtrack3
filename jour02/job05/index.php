a63daca1baa3bfdc6389cf244f87f51b165e78e7
Guillaume <gui.grc@yahoo.com>
2021-03-09 11:44:24 +0100

    création job04 job05 et job06

---
+++ jour02/job05/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Job04</title>
+    <script type='text/javascript' src='script.js'></script>
+</head>
+<body>
+    <textarea id="keylogger" name="story" rows="5" cols="50">
+
+    </textarea>
+    <script></script>
+</body>
+</html>
---
+++ jour02/job06/script.js
@@ -0,0 +1,9 @@
+//Créez un textarea dont l’id est “keylogger”. Chaque fois que l’utilisateur
+// tape une lettre sur son clavier (a-z), celle-ci est ajoutée dans le textarea
+// (même si le focus en cours n’est pas le textarea). Si c’est le cas, la lettre
+// doit être ajoutée deux fois.
+
+function dontClickOnMe(){
+    
+    let textarea =
+}
