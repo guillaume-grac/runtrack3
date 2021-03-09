91f85988d2ec255e7e04c5e20e569b3d1cf4d106
Guillaume <gui.grc@yahoo.com>
2021-03-09 10:27:58 +0100

    job01 Done !

---
+++ jour02/job01/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Job01</title>
+    <script type='text/javascript' src='script.js'></script>
+</head>
+<body>
+    <article id="citation">
+        "La vie a beaucoup plus d’imagination que nous”.
+    </article>
+    <button id="button"> Click ! </button>
+    <script>citation()</script>
+</body>
+</html>
---
+++ jour02/job01/script.js
@@ -0,0 +1,10 @@
+function citation(){
+
+    let button = document.getElementById("button");
+
+    button.onclick = function showArticle() {
+
+        let citation = document.getElementById("citation");
+        console.log(citation);
+    }
+}
