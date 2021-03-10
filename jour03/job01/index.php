24d221f5a27611ca985af05ac812dce30e82ee59
Guillaume <gui.grc@yahoo.com>
2021-03-10 09:53:40 +0100

    job01 Done !

---
+++ jour03/job01/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Job01</title>
+</head>
+<body>
+<button id="button"> Click ! </button>
+<button id="button2"> Don't Click ! </button>
+<div id="myDiv"></div>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script type='text/javascript' src='script.js'></script>
+</body>
+</html>
---
+++ jour03/job01/script.js
@@ -0,0 +1,7 @@
+$('#button').click(function(){
+    $('#myDiv').append('<p>“le $ est un bon élément monétaire. Le # de twitter est une bonne I D. Il faudra faire le point avec la classe pour cacher cet élément."</p>');
+});
+
+$('#button2').click(function (){
+    $('#myDiv').empty();
+});
