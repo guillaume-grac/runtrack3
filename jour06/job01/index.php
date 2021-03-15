94e9eea10515ce571f06d7d98651aed0ba5cd7dd
Guillaume <gui.grc@yahoo.com>
2021-03-15 11:17:49 +0100

    Jour06 Job01 Done

---
+++ jour06/job01/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Job01</title>
+    <link rel="stylesheet" href="style1.css">
+    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="style3.css">
+    <link rel="stylesheet" href="style4.css">
+</head>
+<body>
+
+</body>
+</html>
