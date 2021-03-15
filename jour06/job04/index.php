2693d1d18ac240c9a1777661954373c3e1e56696
Guillaume <gui.grc@yahoo.com>
2021-03-15 15:17:34 +0100

    Job04 Done ! (à revoir tout de même)

---
+++ jour06/job04/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Job04</title>
+    <link rel="stylesheet" media="screen" href="style.css">
+</head>
+<body>
+    <header class="header">
+        <input class="menu-btn" type="checkbox" id="menu-btn" />
+        <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+        <ul class="menu">
+            <li><a>Accueil</a></li>
+            <li><a>Présentation</a></li>
+            <li><a>Contact</a></li>
+        </ul>
+    </header>
+</body>
+</html>
