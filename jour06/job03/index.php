2c9254761aa1b3c3e547feaef80c9d719d763eb9
Guillaume <gui.grc@yahoo.com>
2021-03-15 14:02:50 +0100

    job03 Done !

---
+++ jour06/job03/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Job03</title>
+    <link rel="stylesheet" media="screen" href="style.css">
+</head>
+<body>
+    <section class="section"></section>
+    <section class="section"></section>
+    <section class="section"></section>
+    <section class="section"></section>
+</body>
+</html>
